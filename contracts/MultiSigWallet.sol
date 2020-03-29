931c0fde5ced4b0373e2d4c870510a29ad299cb9
t4sk <tsk.nakamura@gmail.com>
2020-03-29 18:56:33 +0900

    solution - constructor

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -28,5 +28,22 @@ contract MultiSigWallet {
     5. Set the state variable numConfirmationsRequired from the input.
     */
     constructor(address[] memory _owners, uint _numConfirmationsRequired) public {
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,
+            "invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+
+        numConfirmationsRequired = _numConfirmationsRequired;
     }
 }
