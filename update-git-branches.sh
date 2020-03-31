8973e112ab95a522d154e970f33626f0a247cfbc
t4sk <tsk.nakamura@gmail.com>
2020-03-31 18:08:20 +0900

    git script

---
+++ update-git-branches.sh
@@ -0,0 +1,27 @@
+# to update git branch
+# git branch -f branch_name commit_hash
+
+git checkout exercise-00
+git push --force
+git checkout exercise-00-solution
+git push --force
+git checkout exercise-01
+git push --force
+git checkout exercise-01-solution
+git push --force
+git checkout exercise-02
+git push --force
+git checkout exercise-02-solution
+git push --force
+git checkout exercise-03
+git push --force
+git checkout exercise-03-solution
+git push --force
+git checkout exercise-04
+git push --force
+git checkout exercise-04-solution
+git push --force
+git checkout exercise-05
+git push --force
+git checkout exercise-05-solution
+git push --force
